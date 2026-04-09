6f450c778da1783d5391ae9c23bfd60c2714b4bb
mateus124 <alvesmateusq00@gmail.com>
2026-04-08 23:24:13 -0300

    fix: todo card menu

--- src/components/CardMenu/CardMenu.jsx
+++ src/components/CardMenu/CardMenu.jsx
@@ -3,20 +3,30 @@ import { MdOutlineDeleteOutline } from 'react-icons/md';
 import styles from './CardMenu.module.css';
 
 const CardMenu = ({ onOpenAllLinks, onEditBoard, onDeleteBoard }) => {
+    const canOpenAllLinks = typeof onOpenAllLinks === 'function';
+    const canEditBoard = typeof onEditBoard === 'function';
+    const canDeleteBoard = typeof onDeleteBoard === 'function';
+
     return (
         <div className={styles.menu}>
-            <button type="button" className={styles.menuItem} onClick={onOpenAllLinks}>
-                <FiExternalLink size={16} />
-                <span>Abrir todos os links</span>
-            </button>
-            <button type="button" className={styles.menuItem} onClick={onEditBoard}>
-                <FiEdit3 size={16} />
-                <span>Editar card</span>
-            </button>
-            <button type="button" className={`${styles.menuItem} ${styles.menuItemDanger}`} onClick={onDeleteBoard}>
-                <MdOutlineDeleteOutline size={16} />
-                <span>Deletar card</span>
-            </button>
+            {canOpenAllLinks && (
+                <button type="button" className={styles.menuItem} onClick={onOpenAllLinks}>
+                    <FiExternalLink size={16} />
+                    <span>Abrir todos os links</span>
+                </button>
+            )}
+            {canEditBoard && (
+                <button type="button" className={styles.menuItem} onClick={onEditBoard}>
+                    <FiEdit3 size={16} />
+                    <span>Editar card</span>
+                </button>
+            )}
+            {canDeleteBoard && (
+                <button type="button" className={`${styles.menuItem} ${styles.menuItemDanger}`} onClick={onDeleteBoard}>
+                    <MdOutlineDeleteOutline size={16} />
+                    <span>Deletar card</span>
+                </button>
+            )}
         </div>
     );
 };
